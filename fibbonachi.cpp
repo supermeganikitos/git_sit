8b66b8cff74726cade7f70e4d990ff74689c6628
supermeganikitos <nik777555123@gmail.ru>
2026-05-24 00:48:12 +0400

    добавление итеративного фиббоначи (задание 4)

--- fibbonachi.cpp
+++ fibbonachi.cpp
@@ -1,33 +1,30 @@
 
 #include <iostream>
 using namespace std;
-#include <iostream>
-using namespace std;
 
 
 int fib(int n, int first, int last)
 {
     int a;
-    a = last;
-    last += first;
-    first = a;
-    if (n > 0)
+    for(int i =3; i <= n; i ++)
     {
-        return fib(n - 1, first, last);
+        cout << first << "\n";
+        a = last;
+        last += first;
+        first = a;
     }
-    else
-    {
-        return last;
-    }
+    cout << first << "\n";
+    return last;
 }
+
 int main()
 {
-    cout << "Hello world!";
+    cout << "Hello world";
     int n;
     setlocale(LC_ALL, "rus");
-    cout << "фиббоначи рекурсивный \n";
+    cout << "фиббоначи итеративный \n";
     cout << "Введите число";
     cin >> n;
     cout << "\n";
-    cout << n << "-ое число фиббоначи: " << fib(n - 3, 1, 1);
-}
+    cout << n << "-ое число фиббоначи: " << fib(n, 0, 1);
+} 
